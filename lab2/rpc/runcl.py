a707ca1aeff7c781211b2fd6ac7cba55d9eec77e
luk <luk.fabian@gmx.de>
2024-11-14 12:06:44 +0100

    feat(lab2): asynchronous rpc implemented

--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,16 +1,23 @@
 import rpc
 import logging
-
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
 
+
+# Callback-Funktion zur Verarbeitung des Ergebnisses des RPC-Aufrufs
+def response_handler(result):
+    print("Result: {}".format(result.value))
+
+
+# Client-Setup und Start
 cl = rpc.Client()
 cl.run()
 
-base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+# Setzen der Callback-Funktion
+cl.set_response_callback(response_handler)
 
-print("Result: {}".format(result_list.value))
-
-cl.stop()
+# Erstellen der initialen DBList und Starten eines asynchronen Append-Requests mit Timeout
+base_list = rpc.DBList(['foo'])
+cl.append('bar', base_list, timeout=30)
+cl.stop(timeout=30)
--- lab2/rpc/runsrv.py
+++ lab2/rpc/runsrv.py
@@ -1,14 +1,16 @@
 import logging
-
 import rpc
 from context import lab_channel, lab_logging
 
+# Logging-Konfiguration
 lab_logging.setup(stream_level=logging.INFO)
 logger = logging.getLogger('vs2lab.lab2.rpc.runsrv')
 
+# Redis-Channel-Setup
 chan = lab_channel.Channel()
 chan.channel.flushall()
 logger.debug('Flushed all redis keys.')
 
+# Server-Setup und Start
 srv = rpc.Server()
 srv.run()
